1efe03df686cadb7bfc9b6e824beb752b3ed9215
melLedon <mel_ledon@hotmail.com>
2020-01-27 22:02:31 -0600

    actividad dia 5

---
+++ Día 5/scritpt.js
@@ -0,0 +1,54 @@
+//let nombre = prompt("¿Cuál es tu nombre?"); //string o cadena
+let edad = prompt("¿Qué edad tienes?"); //numero
+//let ciudad = prompt("¿En que ciudad vives?");
+let presentacion = "Mi nombre es ";
+let saludo = "Hola ";
+let t = " tienes ";
+let a = " años";
+let residencia = " y vives en "
+//let texto = "Mi nombre es " + nombre + " tengo " + edad + " años" + " y vivo en " + ciudad;
+//let resultado = saludo + nombre + t + edad + a + residencia + ciudad;
+
+
+//console.log(resultado);
+
+// Estructuras de control
+
+if(edad === "25"){
+    console.log("Tu edad es 25");
+} 
+
+if(edad > 18){
+    console.log("Eres mayor de edad");
+} else if(edad < 18){
+    console.log("Eres menor de edad");
+}
+
+
+
+
+
+
+
+// console.log("Mi nombre es " + nombre + " tengo " + edad + " años" + " y vivo en " + ciudad);
+// console.log(texto)
+// console.log(resultado)
+
+
+//function suma (num1, num2){
+    //console.log(num1 + num2);
+    //console.log(num1 * num2);
+//}
+
+//suma(5, 5);
+
+
+
+let booleano = true; //boolean
+let booleano_falso = false; 
+let cars = ["saab","volvo", "bmw"]; //array o arreglo
+let x = {firstName:"john", LastName:"Doe"}; //object u objeto
+let indefinido; //undefined
+
+
+
